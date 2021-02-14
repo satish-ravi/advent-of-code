b15fd0eba975a1614c9221628cd3294c502a3885
Satish Ravi <satishmufc@gmail.com>
2021-02-14 02:52:44 -0800

    add 2019 day19

---
+++ 2019/day19/src/main.rs
@@ -0,0 +1,176 @@
+use std::io::{self, BufRead};
+use std::convert::TryFrom;
+use std::collections::HashMap;
+
+struct Program {
+    memory: Vec<i64>,
+    cur: usize,
+    relative_base: i64
+}
+
+impl Program {
+    fn new(int_code: &[i64]) -> Program {
+        let mut memory = int_code.to_vec();
+        memory.resize(10000, 0);
+        return Program {
+            memory: memory,
+            cur: 0,
+            relative_base: 0
+        }
+    }
+
+    fn get_param_pos(&self, mode: i64, pos: usize) -> usize {
+        if mode == 1 {
+            return pos;
+        } else if mode == 2 {
+            return usize::try_from(self.relative_base + self.memory[pos]).unwrap();
+        } else {
+            return usize::try_from(self.memory[pos]).unwrap();
+        }
+    }
+
+    fn get_param(&self, mode: i64, pos: usize) -> i64 {
+        return self.memory[self.get_param_pos(mode, pos)];
+    }
+
+    fn run_code(&mut self, input: &[i64]) -> Vec<i64> {
+        let mut output = Vec::new();
+        let mut param1: i64;
+        let mut param2: i64;
+        let mut param3: usize;
+        let mut op_code;
+        let mut input_idx = 0;
+
+        loop {
+            op_code = self.memory[self.cur] % 100;
+            if op_code == 99 {
+                return output;
+            }
+            assert!(op_code >= 1 && op_code <= 9);
+            if op_code == 1 || op_code == 2 {
+                param1 = self.get_param((self.memory[self.cur] / 100) % 10, self.cur + 1);
+                param2 = self.get_param((self.memory[self.cur] / 1000) % 10, self.cur + 2);
+                param3 = self.get_param_pos((self.memory[self.cur] / 10000) % 10, self.cur + 3);
+                if op_code == 1 {
+                    self.memory[param3] = param1 + param2;
+                } else {
+                    self.memory[param3] = param1 * param2;
+                }
+                self.cur += 4;
+            } else if op_code == 3 {
+                param3 = self.get_param_pos((self.memory[self.cur] / 100) % 10, self.cur + 1);
+                self.memory[param3] = input[input_idx];
+                self.cur += 2;
+                input_idx += 1;
+            } else if op_code == 4 {
+                output.push(self.get_param((self.memory[self.cur] / 100) % 10, self.cur + 1));
+                self.cur += 2;
+            } else if op_code == 5 || op_code == 6 {
+                param1 = self.get_param((self.memory[self.cur] / 100) % 10, self.cur + 1);
+                param2 = self.get_param((self.memory[self.cur] / 1000) % 10, self.cur + 2);
+                if (op_code == 5 && param1 != 0) || (op_code == 6 && param1 == 0) {
+                    self.cur = usize::try_from(param2).unwrap();
+                } else {
+                    self.cur += 3;
+                }
+            } else if op_code == 7 || op_code == 8 {
+                param1 = self.get_param((self.memory[self.cur] / 100) % 10, self.cur + 1);
+                param2 = self.get_param((self.memory[self.cur] / 1000) % 10, self.cur + 2);
+                param3 = self.get_param_pos((self.memory[self.cur] / 10000) % 10, self.cur + 3);
+                if (op_code == 7 && param1 < param2) || (op_code == 8 && param1 == param2) {
+                    self.memory[param3] = 1;
+                } else {
+                    self.memory[param3] = 0;
+                }
+                self.cur += 4;
+            } else {
+                self.relative_base += self.get_param((self.memory[self.cur] / 100) % 10, self.cur + 1);
+                self.cur += 2;
+            }
+        }
+    }
+}
+
+fn read_data() -> Vec<i64> {
+    let stdin = io::stdin();
+    return stdin.lock().lines().next().unwrap().unwrap().split(",").map(|x| x.parse::<i64>().unwrap()).collect();
+}
+
+fn create_and_run_program(int_code: &[i64], input: &[i64], memory: &mut HashMap<(i64, i64), i64>) -> i64 {
+    if !memory.contains_key(&(input[0], input[1])) {
+        let mut program = Program::new(int_code);
+        let output = program.run_code(input);
+        memory.insert((input[0], input[1]), output[0]);
+        return output[0];
+    }
+    return *memory.get(&(input[0], input[1])).unwrap();
+}
+
+fn part1(int_code: &[i64], memory: &mut HashMap<(i64, i64), i64>) -> usize {
+    let mut affected = 0;
+    for y in 0..50 {
+        for x in 0..50 {
+            let out = create_and_run_program(int_code, &vec![x, y], memory);
+            match out {
+                1 => affected += 1,
+                0 => (),
+                _ => panic!("invalid output")
+            }
+        }
+    }
+    return affected
+}
+
+fn part2(int_code: &[i64], memory: &mut HashMap<(i64, i64), i64>) -> usize {
+    let mut y = 5;
+    let mut x = 0;
+    let santa_size = 100;
+    loop {
+        let mut start_x = x;
+
+        while start_x < x + 100 {
+            if create_and_run_program(int_code, &vec![start_x, y], memory) == 1 {
+                break;
+            }
+            start_x += 1;
+        }
+        if start_x == x + 100 {
+            y += 1;
+            continue;
+        }
+        x = start_x;
+        let mut cur_x = x + 1;
+        loop {
+            if create_and_run_program(int_code, &vec![cur_x, y], memory) == 0 {
+                break;
+            }
+            cur_x += 1;
+        }
+        if cur_x < x + santa_size {
+            y += 1;
+            continue;
+        }
+        let max_x = cur_x - santa_size;
+        for cur_x in x..=max_x {
+            let mut cur_y = y + 1;
+            while cur_y < y + santa_size {
+                if create_and_run_program(int_code, &vec![cur_x, cur_y], memory) == 0 {
+                    break;
+                }
+                cur_y += 1;
+            }
+            if cur_y < y + santa_size {
+                continue;
+            }
+            return (cur_x * 10000 + y) as usize;
+        }
+        y += 1;
+    }
+}
+
+fn main() {
+    let input = read_data();
+    let mut memory = HashMap::new();
+    println!("part1: {}", part1(&input, &mut memory));
+    println!("part2: {}", part2(&input, &mut memory));
+}
