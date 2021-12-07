754a81b4f4654f76a9642b37de3776ab21b9c929
Satish Ravi <satishmufc@gmail.com>
2021-12-06 23:38:56 -0800

    add 2021 day7

---
+++ 2021/day7/solution.py
@@ -0,0 +1,20 @@
+import sys
+from typing import Callable
+
+def read_input() -> list[int]:
+    inp = list(l.strip() for l in sys.stdin.readlines())
+    return [int(x) for x in inp[0].split(",")]
+
+def compute(positions: list[int], cost_fn: Callable[[int], int]) -> int:
+    cur_min = 1000000000000
+    for i in range(min(positions), max(positions)):
+        cur = 0
+        for p in positions:
+            cur += cost_fn(abs(p-i))
+        if cur < cur_min:
+            cur_min = cur
+    return cur_min
+
+inp = read_input()
+print("part1:", compute(inp, lambda n: n))
+print("part2:", compute(inp, lambda n: n * (n + 1) // 2))
